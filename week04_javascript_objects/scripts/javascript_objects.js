858ee527fef58e73f97470c2fa1291514291f10a
sfalabake <sfalabake@byupathway.edu>
2026-01-28 16:21:35 +0100

    Initial commit

---
+++ week04_javascript_objects/scripts/javascript_objects.js
@@ -0,0 +1,38 @@
+// Course object
+let aCourse = {
+    code: "WDD131",
+    title: "Dynamic Web Fundamentals",
+    credits: 2,
+    sections: [
+        { section: "001", enrolled: 95, instructor: "Roberto Diaz Rodriguez" },
+        { section: "002", enrolled: 80, instructor: "Sarah Gobble" }
+    ]
+};
+
+// Set course name
+function setCourseInformation(course) {
+    document.querySelector("#courseName").textContent =
+        `${course.code} – ${course.title}`;
+}
+
+// Render sections table
+function renderSections(course) {
+    const tbody = document.querySelector("#sections tbody");
+    let html = "";
+
+    for (const section of course.sections) {
+        html += `
+      <tr>
+        <td>${section.section}</td>
+        <td>${section.enrolled}</td>
+        <td>${section.instructor}</td>
+      </tr>
+    `;
+    }
+
+    tbody.innerHTML = html;
+}
+
+// Run functions
+setCourseInformation(aCourse);
+renderSections(aCourse);
