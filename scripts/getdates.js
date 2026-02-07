bcf983c98bf99899f51949782759ef9366f2bef6
sfalabake <sfalabake@byupathway.edu>
2026-02-07 20:36:24 +0100

    Corrections on all codes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,16 @@ yearSpan.textContent = new Date().getFullYear();
 // Get last modified date
 document.getElementById("lastModified").innerHTML =
   "Last Modification: " + document.lastModified;
+
+// scripts/getdates.js
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.querySelector("#year");
+  const lastModSpan = document.querySelector("#lastModified");
+
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+  if (lastModSpan) {
+    lastModSpan.textContent = document.lastModified;
+  }
+});
